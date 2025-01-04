db061377d035081d89024027b41fcb87fe917665
snow-zen <2372912451zxj@gmail.com>
2025-01-04 18:09:10 +0800

    feat: 添加 eslint 依赖

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,6 @@
-import type {Route} from "./+types/home";
 import TaskPage from "~/tasks/page"
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     {title: "Auto-animation"},
     {name: "description", content: "auto-animation project"},
